b4bf97adb9ad4cf9a491cb0d09b4c88e18870475
dweihang <dweihang@hotmail.com>
2014-07-06 23:25:25 +0800

    Initial Commit

---
+++ src/com/game/core/GameView.java
@@ -0,0 +1,463 @@
+package com.game.core;
+
+import java.util.HashMap;
+
+import com.game.beta.R;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.SparseIntArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.FrameLayout;
+
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+public class GameView extends FrameLayout {
+
+	public interface GameState{
+		void onGameOver();
+		void onGameWin();
+		void onGameScoreChanged(int score);
+		void onGameMaxTile(int tileVal);
+	}
+	
+	private FrameLayout animLayer;
+	private LinearLayout tileLayer;
+	private Grid grid;
+	private HashMap<Integer,Integer> tileViewId;
+	private HashMap<Integer,Integer> animViewId;
+	private int tileSize;
+	private Handler mHandler;
+	private GameState gs; 
+	
+	private int score;
+	private int maxTileVal;
+	
+	private final int[] icons = {R.drawable.wen, R.drawable.jiao,
+			R.drawable.jing, R.drawable.ou, R.drawable.re,
+			R.drawable.shuang, R.drawable.wa, R.drawable.xia,
+			R.drawable.xia2, R.drawable.xian, R.drawable.xiao,
+			R.drawable.you
+	};
+	
+    public GameView(Context context) {
+		super(context);
+		this.grid  = new Grid(4);
+		this.score = 0;
+		this.maxTileVal = -1;
+		this.tileViewId = new HashMap<Integer,Integer>();
+		this.animViewId = new HashMap<Integer,Integer>();
+		this.mHandler = new Handler();
+		this.initGameView(context);
+		this.setOnTouchListener(new OnSwipeListener(this.getContext()){
+			@Override
+			public void onSwipeRight() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_RIGHT);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_RIGHT);
+		    }
+			@Override
+			public void onSwipeLeft() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_LEFT);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_LEFT);
+		    }
+			@Override
+			public void onSwipeTop() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_TOP);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_TOP);
+		    }
+			@Override
+			public void onSwipeBottom() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_BOTTOM);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_BOTTOM);
+		    }			
+		});
+	}
+	
+    public GameView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		this.grid  = new Grid(4);
+		this.tileViewId = new HashMap<Integer,Integer>();
+		this.animViewId = new HashMap<Integer,Integer>();
+		this.mHandler = new Handler();
+		this.initGameView(context);
+		this.setOnTouchListener(new OnSwipeListener(this.getContext()){
+			@Override
+			public void onSwipeRight() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_RIGHT);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_RIGHT);
+		    }
+			@Override
+			public void onSwipeLeft() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_LEFT);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_LEFT);
+		    }
+			@Override
+			public void onSwipeTop() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_TOP);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_TOP);
+		    }
+			@Override
+			public void onSwipeBottom() {
+				int mergedScore = grid.merge(Grid.MERGE_DIRECTION_BOTTOM);
+				if (mergedScore>0){
+					score += mergedScore;
+					if (gs!=null)gs.onGameScoreChanged(score);
+				}
+				if (grid.checkGameState() == Grid.GAME_OVER && gs!=null){
+					gs.onGameOver();
+				}
+				else if (grid.checkGameState() == Grid.GAME_WIN && gs!=null){
+					gs.onGameWin();
+				}
+				int max = grid.getMax();
+				if (max>maxTileVal){ 
+					maxTileVal = max;
+					if (gs!=null)gs.onGameMaxTile(max);
+				}
+				animate(grid.getShouldMoveIndex(),grid.getShouldMoveVal(),Grid.MERGE_DIRECTION_BOTTOM);
+		    }			
+		});
+	}
+ 
+    @Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		// TODO Auto-generated method stub
+		super.onSizeChanged(w, h, oldw, oldh);
+		tileSize = (Math.min(w, h))/grid.columnCount();
+
+		addTiles(tileSize,tileSize);
+		
+		for (int i=0;i<2;i++){
+			grid.randomAvailCell(2);
+		}
+		this.updateTileView();
+	}
+
+    public void setOnGameStateListener(GameState gs){
+    	this.gs = gs;
+    }
+
+	private void initGameView(Context ctx){
+    	this.tileLayer = new LinearLayout(ctx);
+    	this.animLayer = new FrameLayout(ctx);
+    	this.tileLayer.setOrientation(LinearLayout.VERTICAL);
+    	FrameLayout.LayoutParams animlp = 
+    			new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    	LinearLayout.LayoutParams tilelp = 
+    			new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+    	this.addView(tileLayer,animlp);
+    	this.addView(animLayer,tilelp);
+    }
+    
+    @SuppressLint("NewApi")
+	private void addTiles(int tile_w, int tile_h){
+    	
+    	 LinearLayout line;
+		 LinearLayout.LayoutParams lineLp;
+    	 for(int i=0;i<grid.rowCount();i++){
+    		 line = new LinearLayout(getContext());
+    		 lineLp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,tile_h);
+    		 tileLayer.addView(line,lineLp);
+    		 
+    		 for (int j=0;j<grid.columnCount();j++){
+              ImageView image = new ImageView(getContext());  
+              if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+
+              	int id = Util.generateViewId();
+              	tileViewId.put(i*grid.rowCount()+j, id);
+                image.setId(id);
+
+              } else {
+
+              	int id = View.generateViewId();
+              	tileViewId.put(i*grid.rowCount()+j, id);
+                image.setId(id);
+
+              }
+              image.setImageResource(icons[0]);
+              image.setPadding(3, 3, 3, 3);
+              image.setBackgroundColor(Color.argb(255, 0xbb, 0xad, 0xa0));
+              ViewGroup.LayoutParams lp =new ViewGroup.LayoutParams(tile_w,tile_h);
+           
+              image.setLayoutParams(lp);
+              line.addView(image);
+    		 }
+          }  
+    }
+    
+    private void updateTileView(){
+ 
+		for (int i=0;i<grid.columnCount()*grid.rowCount();i++){
+			int val = grid.getCellVal(i);
+			int imageIndex=0;
+			
+			while (val>1){
+				val = val/2;
+				imageIndex++;
+			}
+			 
+			Integer indexObj = tileViewId.get(i);
+			ImageView img = (ImageView)findViewById(indexObj.intValue());
+			img.setImageResource(icons[imageIndex]);
+			img.setPadding(3, 3, 3, 3);
+			img.setBackgroundColor(Color.argb(255, 0xbb, 0xad, 0xa0));
+	
+			if (i == grid.getCurrentRandomTileIndex()){
+				Animation a = new RotateAnimation(0.0f, 360.0f,
+		                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+		                0.5f);
+		        a.setDuration(300);
+		        img.startAnimation(a);
+			}	
+		}
+    }
+    
+    public void reset(){
+    	grid.empty();
+    	for (int i=0;i<2;i++){
+			grid.randomAvailCell(2);
+		}
+		this.updateTileView();
+		this.animViewId.clear();
+		this.score = 0;
+		mHandler.post(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				animLayer.removeAllViews();
+			}
+			
+		});
+    }
+    
+    @SuppressLint("NewApi")
+	private void animate(SparseIntArray index, SparseIntArray value,int direction){
+		SparseIntArray tmpIdx = index.clone();
+		SparseIntArray tmpVal = value.clone();
+		int step;
+		
+		if (direction == Grid.MERGE_DIRECTION_LEFT || direction == Grid.MERGE_DIRECTION_RIGHT)
+			step = 1;
+		else step = grid.columnCount();
+		
+		for (int i=0;i<tmpIdx.size();i++){
+			int fromCell = tmpIdx.keyAt(i);
+			int toCell = tmpIdx.get(fromCell);
+			int cellVal = tmpVal.get(fromCell);
+			animateOneTile(fromCell,toCell,cellVal,step);
+		}
+    }
+    
+    
+    @SuppressLint("NewApi")
+	private void animateOneTile(final int fromCell, final int toCell, int cellVal,int step){
+    	
+    	LayoutParams lp = new LayoutParams(this.tileSize,this.tileSize); 	
+    	ImageView imgV = new ImageView(getContext());
+    	
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+          	int id = Util.generateViewId();
+          	animViewId.put(fromCell, id);
+            imgV.setId(id);
+
+        } else {
+          	int id = View.generateViewId();
+          	animViewId.put(fromCell, id);
+            imgV.setId(id);
+        }
+		
+		int imageIndex = 0;
+		while (cellVal>1){
+			cellVal = cellVal/2;
+			imageIndex++;
+		}
+		imgV.setImageResource(icons[imageIndex]);
+		imgV.setPadding(3, 3, 3, 3);
+		imgV.setBackgroundColor(Color.argb(255, 0xbb, 0xad, 0xa0));	
+		lp.leftMargin = fromCell%grid.columnCount()*tileSize;
+		lp.topMargin = fromCell/grid.rowCount()*tileSize;
+		imgV.setLayoutParams(lp);
+		animLayer.addView(imgV);
+    	
+		TranslateAnimation ta;
+		if (step>1){
+			ta = new TranslateAnimation(0, 0 , 0, tileSize*(toCell-fromCell)/step);
+		}
+		else {
+			ta = new TranslateAnimation(0,tileSize*(toCell-fromCell), 0 , 0);
+		}
+		ta.setDuration(100);
+		ta.setAnimationListener(new Animation.AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation animation) {
+				int imgResId = tileViewId.get(fromCell);
+				ImageView imgV = (ImageView)tileLayer.findViewById(imgResId);
+				imgV.setImageResource(icons[0]);
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animation animation) {}
+			
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				
+				int animId = animViewId.get(fromCell);
+				animViewId.remove(animId);
+				final ImageView animV = (ImageView)animLayer.findViewById(animId);
+				animV.setVisibility(View.INVISIBLE);
+				int toId = tileViewId.get(toCell);
+				ImageView toV = (ImageView)tileLayer.findViewById(toId);
+				int val = grid.getCellVal(toCell);
+				int imageIndex = 0;
+				while (val>1){
+					val = val/2;
+					imageIndex++;
+				}
+				toV.setImageResource(icons[imageIndex]);
+				
+				
+				int rdnIdx = grid.getCurrentRandomTileIndex();
+				int rdnVal = grid.getCellVal(rdnIdx);
+				ImageView rdnV = (ImageView)tileLayer.findViewById(tileViewId.get(rdnIdx));
+				int rdnImgIdx = 0;
+				while (rdnVal>1){
+					rdnVal = rdnVal/2;
+					rdnImgIdx++;
+				}
+				rdnV.setImageResource(icons[rdnImgIdx]);
+				Animation a = new RotateAnimation(0.0f, 360.0f,
+		                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+		                0.5f);
+		        a.setDuration(300);
+				rdnV.startAnimation(a);
+				animViewId.remove(fromCell);
+				mHandler.post(new Runnable(){
+					public void run() {
+						animLayer.removeView(animV);
+				    }		
+				});
+				
+			}
+		});
+		imgV.startAnimation(ta);
+    }
+}
--- src/com/game/core/Grid.java
+++ src/com/game/core/Grid.java
@@ -13,12 +13,15 @@ public class Grid {
 	private int row;
 	private int max;
 	private int randomTileIndex;
+	private int targetTileVal;
 	
 	private SparseIntArray occupied_index;
 	
-	
+	private SparseIntArray should_move_index;
+	private SparseIntArray should_move_val;
 	
 	public static final int CELL_EMPTY = 0;
+	
 	public static final int INVALID_RANDOM_INDEX=-1;
 	
 	public static final int MERGE_DIRECTION_LEFT = 0;
@@ -29,63 +32,110 @@ public class Grid {
 	public static final int MERGE_INVALID_MAX = -2;
 	public static final int MERGE_FAIL = -3;
 	public static final int MERGE_NO_OP = -1;
+	
+	public static final int GAME_WIN = 1;
+	public static final int GAME_ON = 0;
+	public static final int GAME_OVER = -4;
+	
 	public static final int TARGET_TILE_VAL = 2048;
 	
-	public Grid (int size, int target_val){
+	//probability of generating a 2 or a 4
+	public static int RANDOM_MIN_TILE_PROBABILITY = 60;
+	
+	
+	public Grid (int size){
 		if (size<0)
 			throw new Error("Grid size cannot be negative");
 		this.capacity = size*size;
 		this.column = size;
 		this.row = size;
 		this.max = 0;
+		this.targetTileVal = Grid.TARGET_TILE_VAL;
 		this.randomTileIndex = Grid.INVALID_RANDOM_INDEX;
 		this.occupied_index = new SparseIntArray(this.capacity){
 			public int get(int key){
 				return this.get(key,CELL_EMPTY);
 			}
 		};
+		this.should_move_index = new SparseIntArray(this.capacity){
+			public int get(int key){
+				return this.get(key,CELL_EMPTY);
+			}
+		};
+		this.should_move_val = new SparseIntArray(this.capacity){
+			public int get(int key){
+				return this.get(key,CELL_EMPTY);
+			}
+		};
 	}
 	
-	public void fillCell(int row,int column,int val){
+	public Grid(int row, int column){
+		if (row<0 || column<0)
+			throw new Error("Grid size cannot be negative");
+		this.capacity = row*column;
+		this.column = row;
+		this.row = column;
+		this.max = 0;
+		this.targetTileVal = Grid.TARGET_TILE_VAL;
+		this.randomTileIndex = Grid.INVALID_RANDOM_INDEX;
+		this.occupied_index = new SparseIntArray(this.capacity){
+			public int get(int key){
+				return this.get(key,CELL_EMPTY);
+			}
+		};
+		this.should_move_index = new SparseIntArray(this.capacity){
+			public int get(int key){
+				return this.get(key,CELL_EMPTY);
+			}
+		};
+		this.should_move_val = new SparseIntArray(this.capacity){
+			public int get(int key){
+				return this.get(key,CELL_EMPTY);
+			}
+		};
+	}
+	
+	protected void fillCell(int row,int column,int val){
 		if (this.column<1 && this.row<1){
-			throw new Error("error0");
+			throw new Error("Initialize error");
 		}
 		else if (row<0 || row>this.row || column<0 || column>this.column){
-			throw new Error("err1");
+			throw new Error("Function argument error");
 
 		}
 		else if (val<0){
-			throw new Error("err2");
+			throw new Error("Function argument error");
 		}
 		else {
 			 fillCell((row-1)*this.column+column-1,val);
 		}
 	}
 	
-	public void fillCell(int index,int value){
-		if (index>this.capacity)throw new Error("err1");
+	
+	protected void fillCell(int index,int value){
+		if (index>this.capacity)throw new Error("Function argument error");
 		else {
 			this.occupied_index.put(index,value);
 		}
 	}
 	
-	public boolean eraseCell(int row,int column,int val){
+	protected boolean eraseCell(int row,int column,int val){
 		if (this.column<1 && this.row<1){
-			throw new Error("error0");
+			throw new Error("Initialize error");
 		}
 		else if (row<0 || row>this.row || column<0 || column>this.column){
-			throw new Error("err1");
+			throw new Error("Function argument error");
 
 		}
 		else if (val<0){
-			throw new Error("err2");
+			throw new Error("Function argument error");
 		}
 		else {
 			return eraseCell((row-1)*this.column+column-1);
 		}
 	}
 	
-	public boolean eraseCell(int index){
+	protected boolean eraseCell(int index){
 		if (index>this.capacity)return false;
 		else {
 			this.occupied_index.delete(index);
@@ -93,11 +143,21 @@ public class Grid {
 		}
 	}
 	
-	public void empty(){
-		this.occupied_index.clear();
+	protected void replaceCell(int fromCell,int toCell,int val){
+		should_move_val.put(fromCell, occupied_index.get(fromCell));
+		fillCell(toCell,val);
+		eraseCell(fromCell);
+		should_move_index.put(fromCell,toCell);
+		
 	}
 	
-	public boolean randomAvailCell(int val){
+	protected void empty(){
+		this.occupied_index.clear();
+		this.should_move_index.clear();
+		this.should_move_val.clear();
+	}
+	
+	protected boolean randomAvailCell(int val){
 		if (this.occupied_index.size()>this.capacity){
 			return false;
 		}
@@ -140,10 +200,10 @@ public class Grid {
 	
 	public int getCellVal(int row, int column){
 		if (this.column<1 && this.row<1){
-			throw new Error("error0");
+			throw new Error("Initialize Error");
 		}
 		else if (row<0 || row>this.row || column<0 || column>this.column){
-			throw new Error("err1");
+			throw new Error("Function argument error");
 		}
 		
 		int index = (row-1)*this.column+column-1;
@@ -165,7 +225,10 @@ public class Grid {
 	
 	public int merge(int direction){
         this.randomTileIndex = Grid.INVALID_RANDOM_INDEX;
-		if (max >= TARGET_TILE_VAL)return MERGE_NO_OP;
+        this.should_move_index.clear();
+        this.should_move_val.clear();
+        
+		if (max >= this.targetTileVal)return MERGE_NO_OP;
 		int mergedScore = 0;
 		if (this.occupied_index.size()==0)
 			return MERGE_NO_OP;
@@ -188,15 +251,13 @@ public class Grid {
 								int curr_index_val = getCellVal(curr_index);
 								int j_index_val = getCellVal(offset+j);
 								if (curr_index_val == j_index_val){
-									fillCell(curr_index,curr_index_val*2);
-									eraseCell(offset+j);
+									replaceCell(offset+j,curr_index,curr_index_val*2);			
 									moved = true;
 									mergedScore+=curr_index_val*2;
 									if (curr_index_val*2>max)max = curr_index_val*2;
 								} else {
 									if (curr_index+1 != offset+j){
-										fillCell(curr_index+1,j_index_val);
-										eraseCell(offset+j);
+										replaceCell(offset+j,curr_index+1,j_index_val);					
 										moved = true;
 									}
 									if (j_index_val>max)max = j_index_val;
@@ -209,8 +270,7 @@ public class Grid {
 								if (curr_index_val>max) {
 									max = curr_index_val;
 								}
-							    fillCell(curr_index,curr_index_val);
-							    eraseCell(offset+j);
+								replaceCell(offset+j,curr_index,curr_index_val);
 							    moved = true;
 							}
 						}
@@ -236,15 +296,13 @@ public class Grid {
 								int curr_index_val = getCellVal(curr_index);
 								int j_index_val = getCellVal(offset+j);
 								if (curr_index_val == j_index_val){
-									fillCell(curr_index,curr_index_val*2);
-									eraseCell(offset+j);
+									replaceCell(offset+j,curr_index,curr_index_val*2);
 									moved = true;
 									mergedScore+=curr_index_val*2;
 									if (curr_index_val*2>max)max = curr_index_val*2;
 								} else {
 									if (curr_index-1 != offset+j){
-										fillCell(curr_index-1,j_index_val);
-										eraseCell(offset+j);
+										replaceCell(offset+j,curr_index-1,j_index_val);
 										moved = true;
 									}
 									if (j_index_val>max)max = j_index_val;
@@ -257,8 +315,7 @@ public class Grid {
 								if (curr_index_val>max) {
 									max = curr_index_val;
 								}
-							    fillCell(curr_index,curr_index_val);
-							    eraseCell(offset+j);
+								replaceCell(offset+j,curr_index,curr_index_val);
 							    moved = true;
 							}
 						}
@@ -282,15 +339,13 @@ public class Grid {
 								int curr_index_val = getCellVal(curr_index);
 								int j_index_val = getCellVal(j);
 								if (curr_index_val == j_index_val){
-									fillCell(curr_index,curr_index_val*2);
-									eraseCell(j);
+									replaceCell(j,curr_index,curr_index_val*2);
 									moved = true;
 									mergedScore+=curr_index_val*2;
 									if (curr_index_val*2>max)max = curr_index_val*2;
 								} else {
 									if (curr_index-this.column != j){
-										fillCell(curr_index-this.column,j_index_val);
-										eraseCell(j);
+										replaceCell(j,curr_index-this.column,j_index_val);
 										moved = true;
 									}
 									if (j_index_val>max)max = j_index_val;
@@ -303,8 +358,7 @@ public class Grid {
 								if (curr_index_val>max) {
 									max = curr_index_val;
 								}
-							    fillCell(curr_index,curr_index_val);
-							    eraseCell(j);
+								replaceCell(j,curr_index,curr_index_val);
 							    moved = true;
 							}
 						}
@@ -328,15 +382,13 @@ public class Grid {
 								int curr_index_val = getCellVal(curr_index);
 								int j_index_val = getCellVal(j);
 								if (curr_index_val == j_index_val){
-									fillCell(curr_index,curr_index_val*2);
-									eraseCell(j);
+									replaceCell(j,curr_index,curr_index_val*2);
 									moved = true;
 									mergedScore+=curr_index_val*2;
 									if (curr_index_val*2>max)max = curr_index_val*2;
 								} else {
 									if (curr_index+this.column != j){
-										fillCell(curr_index+this.column,j_index_val);
-										eraseCell(j);
+										replaceCell(j,curr_index+this.column,j_index_val);
 										moved = true;
 									}
 									if (j_index_val>max)max = j_index_val;
@@ -349,8 +401,7 @@ public class Grid {
 								if (curr_index_val>max) {
 									max = curr_index_val;
 								}
-							    fillCell(curr_index,curr_index_val);
-							    eraseCell(j);
+								replaceCell(j,curr_index,curr_index_val);
 							    moved = true;
 							}
 						}
@@ -364,25 +415,71 @@ public class Grid {
 		
 		if (max==MERGE_INVALID_MAX)return MERGE_INVALID_MAX;
 		if (moved){
-			boolean success = randomAvailCell(2);
+			Random rand = new Random();
+			int randVal = rand.nextInt(100);
+			boolean success = false;
+			if (randVal<Grid.RANDOM_MIN_TILE_PROBABILITY){
+				success = randomAvailCell(2);
+			}
+			else {
+				success = randomAvailCell(4);
+			}
+			 
 			if (!success){
-				throw new Error("error0");
+				throw new Error("Generate random tile error");
 			}
 			return mergedScore;
 		}
 		return Grid.MERGE_NO_OP;
 	}
 	
-	public SparseIntArray getAllCells(){
-		return this.occupied_index;
+	public int checkGameState(){
+		if (max>=targetTileVal)return Grid.GAME_WIN;
+		if (this.occupied_index.size()<capacity)return Grid.GAME_ON;
+		for (int i=0;i<capacity;i++){
+			int currTileVal = occupied_index.get(i);
+			int nextTileVal = 
+					((i+1)%this.column==0)?
+					CELL_EMPTY:occupied_index.get(i+1);
+			int downTileVal = occupied_index.get(i+this.column);
+			if (nextTileVal == currTileVal 
+					|| downTileVal == currTileVal)
+				return Grid.GAME_ON;
+		}
+		return Grid.GAME_OVER;
 	}
+	
 	
 	public int getMax(){
 		return this.max;
 	}
 	
+	public int getTargetVal(){
+		return this.targetTileVal;
+	}
+	
+	public void setTargetVal(int val){
+		this.targetTileVal = val;
+	}
+	
+	public SparseIntArray getShouldMoveIndex(){
+		return this.should_move_index;
+	}
+	
+	public SparseIntArray getShouldMoveVal(){
+		return this.should_move_val;
+	}
+	
 	public int getCurrentRandomTileIndex(){
 		return this.randomTileIndex;
+	}
+	
+	public int columnCount(){
+		return this.column;
+	}
+	
+	public int rowCount(){
+		return this.row;
 	}
 	
 }
